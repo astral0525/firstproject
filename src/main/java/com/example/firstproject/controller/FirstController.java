e45c342bb6f664f3c2b0ed07ae94f7c77945adc0
astral0525 <astral0525@gmail.com>
2024-01-09 14:28:14 +0900

    add /bye & layout templates

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -6,12 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
-
     @GetMapping("/hi")
     public String niceToMeetYou(Model model){
         model.addAttribute("username", "홍팍");
         return "greetings"; //templates/greetings.mustache -> 브라우저로 전송
     }
 
+    @GetMapping("/bye")
+    public String seeYouNext(Model model){
+        model.addAttribute("username", "홍팍");
+        return "goodbye"; //templates/greetings.mustache -> 브라우저로 전송
+    }
 
 }
