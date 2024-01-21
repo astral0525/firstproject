3df26f0cfa587158b418a9a549751f660a798125
astral0525 <astral0525@gmail.com>
2024-01-22 06:10:17 +0900

    트ë랜잭션 테스트

--- src/main/java/com/example/firstproject/service/ArticleService.java
+++ src/main/java/com/example/firstproject/service/ArticleService.java
@@ -6,8 +6,10 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service //서비스선언! (서비스 객체를 스프링 부트에 생성)
 @Slf4j
@@ -28,7 +30,7 @@ public class ArticleService {
 
     public Article create(ArticleForm dto) {
         Article article = dto.toEntity();
-        if(article.getId() != null){
+        if (article.getId() != null) {
             return null;
         }
         return articleRepository.save(article);
@@ -44,7 +46,7 @@ public class ArticleService {
         Article target = articleRepository.findById(id).orElse(null);
 
         //3. 잘못된 요청 처리(대상이 없거나, id가 다른 경우)
-        if(target == null|| id != article.getId()){
+        if (target == null || id != article.getId()) {
             return null;
         }
 
@@ -60,7 +62,7 @@ public class ArticleService {
         Article target = articleRepository.findById(id).orElse(null);
 
         //2. 잘못된 요청 처리(대상이 없는 경우)
-        if (target == null){
+        if (target == null) {
             return null;
         }
 
@@ -69,5 +71,26 @@ public class ArticleService {
         return target;
 
 
+    }
+
+    @Transactional //해당 메소드를 트랜잭션으로 묶는다
+    public List<Article> createArticles(List<ArticleForm> dtos) {
+        // dto묶음을 entity 묶음으로 변환
+        List<Article> articleList = dtos.stream()
+                                        .map(dto -> dto.toEntity())
+                                        .collect(Collectors.toList());
+
+        //entity 묶음을 DB로 저장
+        articleList.stream()
+                .forEach(article -> articleRepository.save(article));
+
+
+        //강제 예외 발생
+        articleRepository.findById(-1L).orElseThrow(
+            () -> new IllegalArgumentException("결재실패!")
+        );
+
+        //결과값 반환
+        return articleList;
     }
 }
